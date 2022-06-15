bccacca7138164e20961ab02208d06da6fb34735
sherboboyev-azizbek <azizkhannajot7@gmail.com>
2022-06-15 23:49:57 +0500

    js masala

---
+++ js/main.js
@@ -0,0 +1,198 @@
+// ----------------------------1-mashq
+
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
+// ---------------------------yechim
+
+// function addition(a, b) {
+// 	return a + b;
+// }
+// console.log(addition(10,20))
+
+
+
+
+// ---------------------------2-mashq
+
+// convert(5) ➞ 300
+
+// convert(3) ➞ 180
+
+// convert(2) ➞ 120
+
+// ---------------------------yechim
+
+// function convert(minutes) {
+// 	return minutes * 60;
+// }
+// console.log(convert(10))
+
+
+
+
+// ---------------------------3-mashq
+
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+// ---------------------------yechim
+
+// function addition(num) {
+// 	return num = num +1;
+// }
+// console.log(addition(10))
+
+
+
+
+// ---------------------------4-mashq
+
+// Misol sharti shundaki (x,y) bulsa z= x * y/2
+// triArea(3, 2) ➞ 3    
+
+// triArea(7, 4) ➞ 14
+
+// triArea(10, 10) ➞ 50
+
+// ---------------------------yechim
+
+// function triArea(base, height) {
+// 	return base * height/2;
+// }
+//  console.log(triArea(7,4))
+
+
+
+
+// ---------------------------5-mashq
+
+// calcAge(65) ➞ 23725
+
+// calcAge(0) ➞ 0
+
+// calcAge(20) ➞ 7300
+
+// ---------------------------yechim
+
+// function calcAge(age) {
+// 	return age * 365;
+// }
+
+
+
+// ---------------------------6-mashq
+
+// remainder(1, 3) ➞ 1
+
+// remainder(3, 4) ➞ 3
+
+// remainder(-9, 45) ➞ -9
+
+// remainder(5, 5) ➞ 0
+
+// ---------------------------yechim
+
+// function remainder(x, y) {
+// 	return x % y;
+// }
+
+
+
+
+// ---------------------------7-mashq
+
+// wins get 3 points
+// draws get 1 point
+// losses get 0 points
+
+
+// footballPoints(3, 4, 2) ➞ 13
+
+// footballPoints(5, 0, 2) ➞ 15
+
+// footballPoints(0, 0, 1) ➞ 0
+
+// ---------------------------yechim
+
+// function footballPoints(wins, draws, losses) {
+// 	return (wins * 3 + draws * 1 + losses * 0) ;
+// }
+// console.log(footballPoints(4,2,0));
+
+
+
+
+// ---------------------------8-mashq
+
+// points(1, 1) ➞ 5
+
+// points(7, 5) ➞ 29
+
+// points(38, 8) ➞ 100
+
+// ---------------------------yechim
+
+// function points(twoPointers, threePointers) {
+// 	return (twoPointers * 2 + threePointers * 3);
+// }
+
+
+
+
+// ---------------------------9-mashq
+
+
+// lessThan100(22, 15) ➞ true
+// // 22 + 15 = 37
+
+// lessThan100(83, 34) ➞ false
+// // 83 + 34 = 117
+
+// ---------------------------yechim
+
+// lessThan100(3, 77) ➞ true
+
+// function lessThan100(a, b) {
+// 	if ( a + b <= 100) {
+// 		return true
+// 	}else{
+// 		return false
+// 	}
+	
+// }
+
+// ---------------------------10-mashq
+
+// chickens = 2 legs
+// cows = 4 legs
+// pigs = 4 legs
+
+
+// ---------------------------yechim
+
+// function animals(chickens, cows, pigs) {
+// 	return chickens * 2 + cows * 4 + pigs * 4 ;
+	
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
